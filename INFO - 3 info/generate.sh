bea02c01728ef1284f0b5f139cfcbe4147ad4eda
Gallina Roberto <gallinar00@gmail.com>
2026-09-18 10:16:09 +0200

    Add info

---
+++ INFO - 3 info/generate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pdflatex --shell-escape ./"INFO - 3 info.tex"
+# rm -f *.toc *.log *.aux *.fls *.fdb_latexmk *.out *.lof *.lot *.bbl *.blg *.synctex.gz *.nav *.snm *.vrb *.dvi *.run.xml *.bcf *.idx *.ilg *.ind *.ptc
