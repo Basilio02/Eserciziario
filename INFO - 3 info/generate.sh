f85b1959c1a77b5946600079ead34eb197ff5d19
Gallina Roberto <43840272+Zexal0807@users.noreply.github.com>
2026-09-20 12:58:41 +0200

    Prepare for solutions

--- INFO - 3 info/generate.sh
+++ INFO - 3 info/generate.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 pdflatex --shell-escape ./"INFO - 3 info.tex"
-# rm -f *.toc *.log *.aux *.fls *.fdb_latexmk *.out *.lof *.lot *.bbl *.blg *.synctex.gz *.nav *.snm *.vrb *.dvi *.run.xml *.bcf *.idx *.ilg *.ind *.ptc
+
+pdflatex --shell-escape --jobname="INFO - 3 info SOLUTION" '\def\soluzioni{1}\input{"INFO - 3 info.tex"}'
+
+# find . -maxdepth 1 -type f \
+#     ! -name '*.tex' \
+#     ! -name '*.pdf' \
+#     ! -name '*.png' \
+#     ! -name "$(basename "$0")" \
+#     -delete
